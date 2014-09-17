4604b3e171f67c92331725b370bf98ec96712a02
Kyle Schomp <kyle@pc079088.(none)>
2014-09-17 14:27:39 +0200

    tested proxies. they are now functional.

--- example/post-awazza-proxy.js
+++ example/post-awazza-proxy.js
@@ -3,24 +3,33 @@ var path = require('path');
 var http2 = require('..');
 var http = require("http");
 
-// Passing bunyan logger (optional)
-options.log = require('../test/util').createLogger('server');
+// Passing bunyan logger to http2
 http2.globalAgent = new http2.Agent({
   log: require('../test/util').createLogger('client')
 });
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-// Creating the server
+// Creating an HTTP1.1 server to listen for incoming requests from Awazza
 var server = http.createServer(function(request, response) {
+  // Determine upstream server from requested URL
   var poptions = require('url').parse(request.url);
+
+//----------TEMPORARY TESTING--------------
+//  poptions.host = "106.186.112.116";
+  poptions.port = 8080;
+//----------TEMPORARY TESTING--------------
+
+  // Send request via HTTP2
+  poptions.protocol = "https:"
   var prequest = http2.request(poptions);
   prequest.end();
 
-  // Receiving the response
-  request.on('response', function(presponse) {  
+  // Receiving the response from upstream server
+  prequest.on('response', function(presponse) {
+	// Pipe response to Awazza  
 	presponse.pipe(response);
   });
 });
 
-server.listen(process.env.HTTP2_PORT || 8080);
+server.listen(process.env.HTTP2_PORT || 3456);
--- example/pre-awazza-proxy.js
+++ example/pre-awazza-proxy.js
@@ -10,23 +10,20 @@ var options = process.env.HTTP2_PLAIN ? {
   cert: fs.readFileSync(path.join(__dirname, '/localhost.crt'))
 };
 
-// Passing bunyan logger (optional)
+// Passing bunyan logger to http2 server
 options.log = require('../test/util').createLogger('server');
-http2.globalAgent = new http2.Agent({
-  log: require('../test/util').createLogger('client')
-});
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-// Creating the server
+// Creating HTTP2 server to listen for incoming requests from client
 var server = http2.createServer(options, function(request, response) {
   var poptions = require('url').parse(request.url);
-  poptions.host = process.env.UP_SERVER; // Location of upstream server, either awazza testing proxy or webserver
+  // Replace upstream server from URL with Awazza endpoint
+  poptions.host = process.env.UP_SERVER; 
   poptions.port = process.env.UP_PORT;
-  // HOST header should be taken care of by the final client
+  // Send HTTP1.1 request to Awazza
   http.get(poptions, function (presponse) {
+	// Pipe the response from Awazza to the client
 	presponse.pipe(response);
   });
 });
 
-server.listen(process.env.HTTP2_PORT || 8080);
+server.listen(process.env.HTTP2_PORT || 2345);
