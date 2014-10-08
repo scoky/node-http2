848eabedcb285ea104af417659c043a218d3aa48
Kyle Schomp <kgs7@case.edu>
2014-10-08 16:50:36 +0200

    changed logging of time to human readable

--- example/post-awazza-proxy.js
+++ example/post-awazza-proxy.js
@@ -15,7 +15,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 // Creating an HTTP1.1 server to listen for incoming requests from Awazza
 var server = http.createServer(function(request, response) {
-  console.log(Date.now()+" Received request: "+request.url+" "+JSON.stringify(request.headers))
+  console.log(Date()+" Received request: "+request.url+" "+JSON.stringify(request.headers))
 
   // Determine upstream server from requested URL
   var poptions = url.parse(request.url);
@@ -51,7 +51,7 @@ var server = http.createServer(function(request, response) {
           response.writeHead('404');
           response.end();
         });
-	console.log(Date.now()+" Received response: "+request.url+" "+presponse.statusCode+" "+JSON.stringify(presponse.headers))
+	console.log(Date()+" Received response: "+request.url+" "+presponse.statusCode+" "+JSON.stringify(presponse.headers))
         response.writeHead(presponse.statusCode, '', http2.convertHeadersFromH2(presponse.headers))
 	// Pipe response to Awazza
        	presponse.pipe(response);
