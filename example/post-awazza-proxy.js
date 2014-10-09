e4031b51fd5d7b72dde519296b30ffcc15fa01bc
Kyle Schomp <kgs7@case.edu>
2014-10-09 16:37:32 +0200

    cleaning up code and commenting

--- example/post-awazza-proxy.js
+++ example/post-awazza-proxy.js
@@ -6,63 +6,78 @@ var http2 = require('..')
 var http = require('http')
 var url = require('url')
 
+var TIMEOUT = 5000 // Give up on requests after 5 secs
+
 // Passing bunyan logger to http2
 http2.globalAgent = new http2.Agent({
   log: require('../test/util').createLogger('client')
 })
 
+// Do not validate server certificate
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 // Creating an HTTP1.1 server to listen for incoming requests from Awazza
 var server = http.createServer(function(request, response) {
+
   console.log(Date()+" Received request: "+request.url+" "+JSON.stringify(request.headers))
 
   // Determine upstream server from requested URL
-  var poptions = url.parse(request.url);
+  var poptions = url.parse(request.url)
 
   // Always send request via HTTP2 over TLS
   poptions.protocol = "https:"
-  poptions.port = 443
-  poptions.href = url.format(poptions)
+  poptions.port = 443  
+  // Convert the http/1.1 headers from Awazza into http/2 headers
   poptions.headers = http2.convertHeadersToH2(request.headers)
+  
+  // Update the href to reflect the content server
   poptions.host = poptions.hostname = poptions.headers[':authority']
+  poptions.href = url.format(poptions)
+
+  // Disable plain text mode
   poptions.plain = false
 
-//  console.log("Sending request: "+JSON.stringify(poptions));
-  var prequest = http2.request(poptions);
-  function onErr(err) {
-        console.log('PRequest error: '+err);
-        response.writeHead('404');
-        response.end();
-  };
-  prequest.on('error', onErr);
-
+  // Send the request to the content server
+  // console.log("Sending request: "+JSON.stringify(poptions))
+  var prequest = http2.request(poptions)
+  prequest.on('error', function (err) {
+    console.log('PRequest error: '+err)
+    // Something went wrong, send Awazza a 502 error
+    response.writeHead(502)
+    response.end()
+  })
   prequest.setTimeout(5000, function () {
-    console.log('PRequest timed out');
+    console.log('PRequest timed out')
+    // Request timedout
     prequest.abort()
+    // Tell Awazza about the timeout
     response.writeHead(504)
     response.end()
   })
 
-  // Receiving the response from upstream server
+  // Receiving the response from content server
   prequest.on('response', function(presponse) {
-        presponse.on('error', function(err) {
-          console.log('PResponse Error: '+err);
-          response.writeHead('404');
-          response.end();
-        });
-	console.log(Date()+" Received response: "+request.url+" "+presponse.statusCode+" "+JSON.stringify(presponse.headers))
-        response.writeHead(presponse.statusCode, '', http2.convertHeadersFromH2(presponse.headers))
-	// Pipe response to Awazza
-       	presponse.pipe(response);
-  });
+    presponse.on('error', function(err) {
+    console.log('PResponse Error: '+err)
+      // Something went wrong, send Awazza a 502 error
+      response.writeHead('502')
+      response.end()
+    })
 
-  prequest.end();
-});
+    console.log(Date()+" Received response: "+request.url+" "+presponse.statusCode+" "+JSON.stringify(presponse.headers))
 
-/*server.on('error', function(err) {
-  console.log('Server error: '+err);
-});*/
+    response.writeHead(presponse.statusCode, '', http2.convertHeadersFromH2(presponse.headers))
+    // Pipe response to Awazza
+    presponse.pipe(response)
+  })
 
-server.listen(process.env.HTTP2_PORT || 2345);
+  prequest.end()
+})
+
+/*server.on('error', function(err) { // This will never catch an error?
+  console.log('Server error: '+err)
+})*/
+
+// Listen on port 2345 by default
+server.listen(process.env.HTTP2_PORT || 2345)
 
--- example/pre-awazza-proxy.js
+++ example/pre-awazza-proxy.js
@@ -1,44 +1,53 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var http2 = require('..');
-var http = require("http");
+var fs = require('fs')
+var path = require('path')
+var http2 = require('..')
+var http = require("http")
 
-//var path_ext = '/h2_awazza'
 var options = process.env.HTTP2_PLAIN ? {
   plain: true
 } : {
+  // Dub certificate to issue to clients
   key: fs.readFileSync(path.join(__dirname, '/localhost.key')),
   cert: fs.readFileSync(path.join(__dirname, '/localhost.crt'))
-};
+}
 
 // Passing bunyan logger to http2 server
-options.log = require('../test/util').createLogger('server');
+options.log = require('../test/util').createLogger('server')
 
 // Creating HTTP2 server to listen for incoming requests from client
 var server = http2.createServer(options, function(request, response) {
-  console.log(Date()+" Received request: "+request.url+" "+JSON.stringify(request.headers));
 
-  var poptions = require('url').parse(request.url);
+  console.log(Date()+" Received request: "+request.url+" "+JSON.stringify(request.headers))
+
+  var poptions = require('url').parse(request.url)
+  // Convert the http/2 headers received from the client into http/1.1 headers for Awazza
   poptions.headers = http2.convertHeadersFromH2(request.headers)
 
-//  poptions.pathname = poptions.pathname.lastIndexOf(path_ext, 0) === 0 ? poptions.pathname.substring(path_ext.length) : poptions.pathname
-//  poptions.headers[':path'] = poptions.headers[':path'].lastIndexOf(path_ext, 0) === 0 ? poptions.headers[':path'].substring(path_ext.length) : poptions.headers[':path']
-
-  // Replace upstream server from URL with Awazza endpoint
-  poptions.host = poptions.hostname = process.env.UP_SERVER || 'localhost'; 
-  poptions.port = process.env.UP_PORT || 8899;
+  // Replace upstream server from URL with Awazza endpoint, default localhost:8899
+  poptions.host = poptions.hostname = process.env.UP_SERVER || 'localhost'
+  poptions.port = process.env.UP_PORT || 8899
+  // Awazza doesn't speak https
   poptions.protocol = 'http:'
  
-  // Send HTTP1.1 request to Awazza
+  // Send http/1.1 request to Awazza
   http.get(poptions, function (presponse) {
+
     console.log(Date()+" Received response: "+request.url+' '+presponse.statusCode+" "+JSON.stringify(presponse.headers))
+
     // Convert and write the headers
     response.writeHead(presponse.statusCode, '', http2.convertHeadersToH2(presponse.headers))
     // Pipe the response from Awazza to the client
-    presponse.pipe(response);
-  });
-});
+    presponse.pipe(response)
+  })
 
-server.listen(process.env.HTTP2_PORT || 4567);
+  response.on('error', function (err) {
+    // Error sending response to client
+    // Nothing to be done except log the event
+    console.log('Response Error: '+err)
+  })
+})
+
+// Listen on port 4567 by default
+server.listen(process.env.HTTP2_PORT || 4567)
