4e45bfc7b4029faf0af434b2575f90f51f2bdb61
Kyle Schomp <kgs7@case.edu>
2014-10-14 12:57:16 +0200

    another fixed for the location header 302 redirect

--- example/client.js
+++ example/client.js
@@ -4,7 +4,6 @@ var fs = require('fs')
 var path = require('path')
 var http2 = require('..')
 
-
 // Logging
 http2.globalAgent = new http2.Agent({
   log: require('../test/util').createLogger('client')
@@ -15,8 +14,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 // Parse argv
 var argv = require('minimist')(process.argv.slice(2))
 if (argv.h || argv._.length != 1) {
-  console.log('USAGE: node client.js <url> [-p proxy:port] [-v] [-h]')
+  console.log('USAGE: node client.js <url> [-p proxy:port] [-v] [-h] [-t times]')
   console.log('-p indicate a HTTP2 TLS proxy to use')
+  console.log('-t number of times to perform the request')
   console.log('-v print status code and headers of response')
   console.log('-h print this help menu')
   process.exit()
@@ -42,37 +42,45 @@ if (argv.p) {
 // Sending the request
 // It would be `var request = http2.get(process.argv.pop());` if we wouldn't care about plain mode
 // console.log('Sending '+JSON.stringify(options, null, '\t'))
-var request = http2.request(options)
-request.end()
+function run() {
+  var request = http2.request(options)
+  request.end()
 
-// Receiving the response
-request.on('response', function(response) {
-  if (argv.v) {
-    console.log('CODE='+response.statusCode)
-    console.log('HEADERS='+JSON.stringify(response.headers, null, '\t')+'\n')
-  }
-  response.pipe(process.stdout)
+  // Receiving the response
+  request.on('response', function(response) {
+    if (argv.v) {
+      console.log('CODE='+response.statusCode)
+      console.log('HEADERS='+JSON.stringify(response.headers, null, '\t')+'\n')
+    }
+    response.pipe(process.stdout)
 
-  response.on('end', finish)
-});
+    response.on('end', finish)
+  })
 
-// Receiving push streams - IGNORE PUSH FOR NOW
-/*request.on('push', function(pushRequest) {
-  var filename = path.join(__dirname, '/push-' + push_count);
-  push_count += 1;
-  console.error('Receiving pushed resource: ' + pushRequest.url + ' -> ' + filename);
-  pushRequest.on('response', function(pushResponse) {
-    pushResponse.pipe(fs.createWriteStream(filename)).on('finish', finish);
-  });
-});*/
+  // Receiving push streams - IGNORE PUSH FOR NOW
+  /*request.on('push', function(pushRequest) {
+    var filename = path.join(__dirname, '/push-' + push_count)
+    push_count += 1
+    console.error('Receiving pushed resource: ' + pushRequest.url + ' -> ' + filename)
+    pushRequest.on('response', function(pushResponse) {
+      pushResponse.pipe(fs.createWriteStream(filename)).on('finish', finish)
+    })
+  })*/
+}
+
+// Perform the first request
+for (var i = 0; i < argv.t; i++) {
+  run()
+}
 
 // Quitting after both the response and the associated pushed resources have arrived
-var push_count = 0
 var finished = 0
 function finish() {
   finished += 1
-  if (finished === (1 + push_count)) {
+  if (finished >= argv.t) {
     console.log('')
     process.exit()
-  }
+  } /*else {
+    run()
+  }*/
 }
