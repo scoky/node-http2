643376d5675fcb196140d7a0ddadde3594f26530
Kyle Schomp <kgs7@case.edu>
2014-11-05 14:18:31 +0100

    added new function

--- example/phase3.py
+++ example/phase3.py
@@ -73,6 +73,7 @@ if __name__ == "__main__":
    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
    parser.add_argument('-d', '--directory', default=None, help='Directory for writing log')
    parser.add_argument('-n', '--numtrials', default=5, type=int, help='number of trials per URL')
+   parser.add_argument('-t', '--threads', default=None, type=int, help='number of threads to use')
    parser.add_argument('-c', '--chunk', default=20, help='chunk size to assign to each thread')
    args = parser.parse_args()
 
