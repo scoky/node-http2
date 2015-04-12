23c5623a68381b707eecd13967b995f474282c37
Kyle Schomp <kgs7@case.edu>
2015-04-12 15:13:03 -0400

    fixed bug

--- example/phase3_pager.py
+++ example/phase3_pager.py
@@ -89,7 +89,7 @@ def output(url, protocol, data):
     args.outfile.write( (url + ' ' + protocol + ' objs_h2=' + str(objects['h2']) + ' objs_spdy=' + str(objects['spdy']) +
         ' objs_h1=' + str(objects['http/1.1']) + ' conns_h2=' + str(conns['h2']) + ' conns_spdy=' + str(conns['spdy']) +
         ' conns_h1=' + str(conns['http/1.1']) + ' domains=' + str(domains) + ' size_h2=' + str(size['h2']) + ' size_spdy=' + 
-        str(size['spdy']) + ' size_h1=' + str(size['http/1.1']) + ' time=' + str(time) + '\n') )
+        str(size['spdy']) + ' size_h1=' + str(size['http/1.1']) + ' push=' + str(push) + ' time=' + str(time) + '\n') )
 
 if __name__ == "__main__":
     # set up command line args
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     data = {'h2':defaultdict(dict), 'http/1.1':defaultdict(dict), 'spdy':defaultdict(dict)}
     for line in args.infile:
         chunks = line.rstrip().split()
-        f = Fetch(chunks[0], chunks[1], chunks[2], chunks[3], bool(chunks[4]), bool(chunks[5]), chunks[6], float(chunks[7]), chunks[8], chunks[9])
+        f = Fetch(chunks[0], chunks[1], chunks[2], chunks[3], chunks[4] == 'True', chunks[5] == 'True', chunks[6], float(chunks[7]), chunks[8], chunks[9])
         if f.key not in data[f.protocol][f.page]:
             data[f.protocol][f.page][f.key] = []
         data[f.protocol][f.page][f.key].append(f)
